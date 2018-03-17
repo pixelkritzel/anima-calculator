315ea12e97ceb637431e043f1e449c3200a6adb1
Timo Zoeller <timo@pixelkritzel.de>
2018-03-17 18:45:42 +0100

    Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,10 @@
 import * as React from 'react';
-import './App.css';
 
-const logo = require('./logo.svg');
+import './stylesheets/App.css';
 
 class App extends React.Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
-    );
+    return <div className="App">Anima Calculator</div>;
   }
 }
 
