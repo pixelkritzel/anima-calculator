79fa0d9b7c0f6d051cf4ed01d6eb9fb716d5c158
Timo Zoeller <timo@pixelkritzel.de>
2018-05-11 21:25:04 +0200

    ...

--- src/components/FightTableRow.tsx
+++ src/components/FightTableRow.tsx
@@ -2,9 +2,11 @@ import * as React from 'react';
 
 import { observer } from 'mobx-react';
 
-import { Button, ButtonGroup } from 'reactstrap';
+import { Button, ButtonGroup, Form, FormGroup, Label, Input } from 'reactstrap';
+import { FaCheck } from 'react-icons/lib/fa';
 
 import { ICharakterInFightModel } from '../store/charakterInFightModel';
+import { observable } from 'mobx';
 
 type ICharactersTableRowProps = {
   character: ICharakterInFightModel;
@@ -14,6 +16,37 @@ type ICharactersTableRowProps = {
 
 @observer
 class CharactersTableRow extends React.Component<ICharactersTableRowProps, {}> {
+  @observable showModifierForm = false;
+
+  @observable
+  modifier = {
+    value: 0,
+    reason: ''
+  };
+
+  renderModifierForm() {
+    return (
+      <Form>
+        <FormGroup>
+          <Label>Modfier value</Label>
+          <Input type="number" onChange={event => (this.modifier.value = parseInt(event.target.value, 10))} />
+        </FormGroup>
+        <FormGroup>
+          <Label>Modfier reason</Label>
+          <Input type="text" onChange={event => (this.modifier.reason = event.target.value)} />
+        </FormGroup>
+        <Button
+          onClick={() => {
+            this.props.character.addModifier(this.modifier);
+            this.showModifierForm = false;
+          }}
+        >
+          <FaCheck /> Add modifier
+        </Button>
+      </Form>
+    );
+  }
+
   render() {
     const { character, index } = this.props;
     return (
@@ -23,22 +56,31 @@ class CharactersTableRow extends React.Component<ICharactersTableRowProps, {}> {
         <td>{character.baseCharacter.group} </td>
         <td>{character.baseCharacter.baseInitiative}</td>
         <td>
-          <ul>
+          {this.showModifierForm ? (
+            this.renderModifierForm()
+          ) : (
+            <Button color="link" onClick={() => (this.showModifierForm = true)}>
+              Add modifier
+            </Button>
+          )}
+          <ul className="list-unstyled">
             {character.modifiers.map((mod, modIndex) => (
-              <li key={`charakter-${index}-modifier-${modIndex}`}>
+              <li key={`charakter-${index}-modifier-${mod.id}`}>
                 {mod.value}
                 {mod.reason && ' - '}
                 {mod.reason}
+                <Button color="link" onClick={() => character.removeModifier(mod.id)}>
+                  &times;
+                </Button>
               </li>
             ))}
           </ul>
         </td>
         <td>{character.d100}</td>
         <td>{character.currentInitiative}</td>
-        <th>nobody</th>
+        <th>{character.advantageAgainst.map(opponent => opponent.baseCharacter.name)}</th>
         <th>
           <ButtonGroup>
-            <Button color="primary">Edit</Button>
             <Button onClick={character.rolld100}>Roll D100</Button>
             <Button color="danger">Delete</Button>
           </ButtonGroup>
--- src/store/charakterInFightModel.ts
+++ src/store/charakterInFightModel.ts
@@ -1,24 +1,37 @@
-import { types } from 'mobx-state-tree';
+import { getParent, types } from 'mobx-state-tree';
 
 import d100 from '../utils/d100';
+import generateUUID from '../utils/generateUUID';
 
 import { characterModel } from './characterModel';
+
+type IModifierData = {
+  id?: string;
+  value: number;
+  reason?: string;
+};
+
+const ModifierModel = types.model({
+  id: types.string,
+  value: types.number,
+  reason: types.optional(types.string, () => '')
+});
 
 export const characterInFightModel = types
   .model('character', {
     baseCharacter: types.reference(characterModel),
-    modifiers: types.optional(
-      types.array(
-        types.model({
-          value: types.number,
-          reason: types.optional(types.string, () => '')
-        })
-      ),
-      () => []
-    ),
+    modifiers: types.optional(types.array(ModifierModel), () => []),
     d100: 0
   })
   .actions(self => ({
+    addModifier(modifierData: IModifierData) {
+      modifierData.id = generateUUID();
+      self.modifiers.push(ModifierModel.create(modifierData));
+    },
+    removeModifier(id: string) {
+      const index = self.modifiers.findIndex(mod => mod.id === id);
+      self.modifiers.splice(index - 1, 1);
+    },
     rolld100() {
       self.d100 = d100();
     }
@@ -27,6 +40,10 @@ export const characterInFightModel = types
     get currentInitiative() {
       const sumOfModifiers = self.modifiers.reduce((prev, mod) => prev + mod.value, 0);
       return self.baseCharacter.baseInitiative + sumOfModifiers + self.d100;
+    },
+    get advantageAgainst() {
+      const allOpponents: ICharakterInFightModel[] = getParent(self);
+      return allOpponents.filter(opponent => opponent.currentInitiative + 150 < this.currentInitiative);
     }
   }));
 
