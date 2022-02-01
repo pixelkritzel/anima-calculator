8c227a8e7dd6eab6525d4de62cd9813f004f900b
Timo Zöller <timo@pixelkritzel.de>
2022-02-01 13:35:50 +0100

    Changes font hosting to local

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,11 @@ import * as ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from 'registerServiceWorker';
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import 'stylesheets/style.css';
 
 ReactDOM.render(
